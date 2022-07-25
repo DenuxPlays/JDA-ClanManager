19bda581cd5f3fc3aad7932b3471211d1e1b39d8
-Denux <timon.klinkert@gmail.com>
2022-07-25 21:47:29 +0200

    updated some java docs

--- src/main/java/dev/denux/clanmanager/core/exceptions/ClanManagerException.java
+++ src/main/java/dev/denux/clanmanager/core/exceptions/ClanManagerException.java
@@ -1,16 +1,12 @@
 package dev.denux.clanmanager.core.exceptions;
 
 /**
- * Runtime Exception that is thrown for everything that is related to DIH4JDA.
+ * Runtime Exception that is thrown for everything that is related to the ClanManager.
  */
 public class ClanManagerException extends RuntimeException {
 
     public ClanManagerException(String message) {
         super(message);
-    }
-
-    public ClanManagerException(String message, Throwable cause) {
-        super(message, cause);
     }
 
     public ClanManagerException(Throwable cause) {
