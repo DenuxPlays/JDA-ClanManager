c03785749a7cce12dc6eff68e95a89c9baa75152
-Denux <timon.klinkert@gmail.com>
2022-09-29 21:38:35 +0200

    bumped version to `1.0.0-beta.1`

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 group = "dev.denux"
 val archivesBaseName = "jda-clanmanager"
-version = "1.0.0-alpha.9"
+version = "1.0.0-beta.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
